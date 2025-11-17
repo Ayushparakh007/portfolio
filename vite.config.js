1ce10485f48e39c1c9abb77e22d905161eeec90e
Ayushparakh007 <AYUSHPARAKH2004@GMAIL.COM>
2025-11-17 21:31:46 +0530

    Fix Three.js circular dependency for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['three', '@react-three/fiber', '@react-three/drei'],
+  },
   build: {
     chunkSizeWarningLimit: 2000,
+    commonjsOptions: {
+      include: [/three/, /node_modules/],
+    },
   }
 })
